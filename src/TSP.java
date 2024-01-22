3a3a7d1a27aef2c1004d7c4dfbcc5479b50f2a76
Laura Haro Escoi <lauraha.escoi@gmail.com>
2024-01-22 17:17:05 +0100

    Me vull morir

--- src/TSP.java
+++ src/TSP.java
@@ -17,6 +17,13 @@ public class TSP
     public static final int CMatrixPading = 3;
 
 
+    // Our methods
+    public Methods method;
+    public int numThreads;
+    public ExecutorService executorService;
+    public ForkJoinPool forkJoinPool;
+
+
     public int DistanceMatrix[][];
     // Priority queue to store live nodes of the search tree
     PriorityQueue<Node> NodesQueue = new PriorityQueue<Node>(Collections.reverseOrder((Node c1, Node c2) -> Integer.compare(c1.getCost(),c2.getCost())));
@@ -55,8 +62,28 @@ public class TSP
     {
         InitDefaultCitiesDistances();
     }
-    public TSP(String citiesPath)
+    public TSP(String citiesPath, int numThreads, Methods method)
     {
+        ReadCitiesFile(citiesPath);
+        this.numThreads = numThreads;
+        this.method = method;
+        switch (method) {
+            case FIXED_THREAD_POOL:
+                executorService = Executors.newFixedThreadPool(this.numThreads);
+                break;
+            case CACHED_THREAD_POOL:
+                executorService = Executors.newCachedThreadPool();
+                break;
+            case FORK_JOIN_POOL:
+                forkJoinPool = new ForkJoinPool(this.numThreads);
+                break;
+            default:
+                System.err.println("[TSP::TSP] Error: Invalid method.");
+                System.exit(-1);
+        }
+    }
+
+    public TSP(String citiesPath) {
         ReadCitiesFile(citiesPath);
     }
 
@@ -98,11 +125,7 @@ public class TSP
             }
 
         } catch (FileNotFoundException e) {
-            System.err.printf("[TSP::ReadCitiesFile] File %s not found.\n",citiesPath);
-            e.printStackTrace();
-        } catch (
-            IOException e) {
-            System.err.printf("[TSP::ReadCitiesFile] Error file reading %s.\n",citiesPath);
+            System.err.printf("[TSP::ReadCitiesFile] File %s not found.\n", citiesPath);
             e.printStackTrace();
         }
     }
@@ -121,30 +144,16 @@ public class TSP
         return solution;
     }
 
-    public void executeMethod(Methods method) throws InterruptedException {
-        int numThreads = NCities;
-        switch(method) {
-            case FIXED_THREAD_POOL:
-                useFixedThreadPool(numThreads);
-                break;
-            case CACHED_THREAD_POOL:
-                useCachedThreadPool(numThreads);
-                break;
-            case FORK_JOIN_POOL:
-                useForkJoinPool(numThreads);
-                break;
-            default:
-                throw new IllegalArgumentException("Método de concurrencia no soportado");
-        }
-    }
+    // Function to solve the traveling salesman problem using Branch and Bound
+    public Node Solve(int CostMatrix[][])
+    {
+        Node min, child;
 
-    private void useFixedThreadPool(int numThreads) throws InterruptedException {
-        ExecutorService executor = Executors.newFixedThreadPool(numThreads);
         System.out.println("\n___________________________________________________________________________________________________________________________________________________");
         System.out.printf("Test with %d cities.\n",getNCities());
 
         // Create a root node and calculate its cost. The TSP starts from the first city, i.e., node 0
-        Node root = new Node(this, DistanceMatrix);
+        Node root = new Node(this, CostMatrix);
         //System.out.println(root);
 
         // Calculate the lower bound of the path starting at node 0
@@ -152,117 +161,6 @@ public class TSP
 
         // Add root to the list of live nodes
         pushNode(root);
-        if(NodesQueue.size() < numThreads) latch = new CountDownLatch(NodesQueue.size());
-        else latch = new CountDownLatch(numThreads);
-
-        // Enviar tareas iniciales al pool
-        while (!NodesQueue.isEmpty()) {
-            System.out.println("Entro al bucle");
-                Node currentNode = popNode();
-                executor.submit(() -> {
-                    processNode(currentNode);
-                    latch.countDown();
-                });
-                latch.await();
-        }
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
-                executor.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executor.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
-        System.out.printf("\nFinal Total nodes: %d \tProcessed nodes: %d \tPurged nodes: %d \tPending nodes: %d \tBest Solution: %d.",root.getTotalNodes(), ProcessedNodes, PurgedNodes, NodesQueue.size(),getSolution()==null?0:getSolution().getCost());
-
-    }
-
-    private void useCachedThreadPool(int numThreads){
-        ExecutorService executor = Executors.newCachedThreadPool();
-
-        for(int i = 0; i < numThreads && !NodesQueue.isEmpty(); ++i){
-            Node currentNode = popNode();
-            executor.submit(() -> processNode(currentNode));
-        }
-        executor.shutdown();
-        try {
-            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
-                executor.shutdownNow();
-            }
-        } catch (InterruptedException e) {
-            executor.shutdownNow();
-            Thread.currentThread().interrupt();
-        }
-    }
-
-    private void useForkJoinPool(int numThreads){
-        ForkJoinPool forkJoinPool = new ForkJoinPool();
-        for(int i = 0; i < numThreads && !NodesQueue.isEmpty(); ++i){
-            Node currentNode = popNode();
-            forkJoinPool.execute(() -> processNode(currentNode));
-        }
-        forkJoinPool.shutdown();
-        try{
-            forkJoinPool.awaitTermination(60, TimeUnit.SECONDS);
-        } catch (InterruptedException e){
-            e.printStackTrace();
-        }
-    }
-
-    private void processNode(Node node) {
-        PriorityQueue<Node> threadQueue = new PriorityQueue<>();
-        // Incrementar el contador de nodos procesados.
-        synchronized (this) {
-            ProcessedNodes++;
-        }
-
-        // i almacena el número de la ciudad actual.
-        int i = node.getVertex();
-
-        // Si todas las ciudades han sido visitadas, es decir, si es una hoja en el árbol de búsqueda.
-        if (node.getLevel() == NCities - 1) {
-            // Completa el ciclo retornando a la ciudad de origen.
-            node.addPathStep(i, 0);
-
-            // Sincroniza el acceso a la solución actual para compararla y posiblemente actualizarla.
-            synchronized (this) {
-                if (getSolution() == null || node.getCost() < getSolution().getCost()) {
-                    setSolution(node);
-                    PurgeWorseNodes(node.getCost());
-                }
-            }
-        } else {
-            // Explora las ciudades no visitadas para crear nodos hijos.
-            for (int j = 0; j < NCities; j++) {
-                System.out.println("Entro a bucle 2");
-                if (!node.cityVisited(j) && node.getCostMatrix(i, j) != INF) {
-                    Node child = new Node(this, node, node.getLevel() + 1, i, j);
-                    int child_cost = node.getCost() + node.getCostMatrix(i, j) + child.calculateCost();
-                    if (getSolution() == null || child_cost < getSolution().getCost()) {
-                        child.setCost(child_cost);
-                        pushNode(child);
-                        //threadQueue.add(child);
-                        System.out.println("Afegeixo child");
-                    }
-                    else if (getSolution()!=null && child_cost>getSolution().getCost()) {
-                        synchronized (this){
-                            PurgedNodes++;
-                            System.out.println("Afegeixo purged");
-                        }
-                    }
-                }
-                System.out.println("Acabo bucle");
-            }
-        }
-        System.out.println("Acabo funció");
-    }
-
-
-    // Function to solve the traveling salesman problem using Branch and Bound
-    public Node Solve(int CostMatrix[][])
-    {
-        Node min, child;
 
         // Pop a live node with the least cost, check it is a solution and adds its children to the list of live nodes.
         while ((min=popNode())!=null) // Pop the live node with the least estimated cost
@@ -270,7 +168,7 @@ public class TSP
             ProcessedNodes++;
             if (true && (min.getTotalNodes()%10000)==0) System.out.printf("Total nodes: %d \tProcessed nodes: %d \tPurged nodes: %d \tPending nodes: %d \tBest Solution: %d\r",min.getTotalNodes(), ProcessedNodes, PurgedNodes, NodesQueue.size(),getSolution()==null?0:getSolution().getCost());
             if (false && (min.getTotalNodes()%10000)==0)  System.out.println(NodesQueue);
-             // i stores the current city number
+            // i stores the current city number
             int i = min.getVertex();
 
             // If all cities are visited
@@ -298,7 +196,7 @@ public class TSP
                     // Create a child node and calculate its cost
                     child = new Node(this, min, min.getLevel() + 1, i, j);
                     int child_cost =    min.getCost() + min.getCostMatrix(i,j) +
-                                        child.calculateCost();
+                            child.calculateCost();
 
                     // Add node to pending nodes queue if its costs is lower than better solution
                     if (getSolution()==null || child_cost<getSolution().getCost())
@@ -313,10 +211,40 @@ public class TSP
             }
         }
 
-        System.out.printf("\nFinal Total nodes: %d \tProcessed nodes: %d \tPurged nodes: %d \tPending nodes: %d \tBest Solution: %d.",min.getTotalNodes(), ProcessedNodes, PurgedNodes, NodesQueue.size(),getSolution()==null?0:getSolution().getCost());
+        if (true) System.out.printf("\nFinal Total nodes: %d \tProcessed nodes: %d \tPurged nodes: %d \tPending nodes: %d \tBest Solution: %d.",min.getTotalNodes(), ProcessedNodes, PurgedNodes, NodesQueue.size(),getSolution()==null?0:getSolution().getCost());
 
         return getSolution();  // Return solution
     }
+
+    public Node Solve_Conc() {
+        Instant start = Instant.now();
+        Node solution = null;
+
+
+        try {
+            solution = Solve_Conc(DistanceMatrix);
+            if (this.method == Methods.FORK_JOIN_POOL) {
+                forkJoinPool.shutdown();
+            } else {
+                executorService.shutdown();
+            }
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+        }
+        printSolution("\nOptimal Solution: ", solution);
+
+        Instant finish = Instant.now();
+        long timeElapsed = Duration.between(start, finish).toMillis();  //in millis
+        System.out.printf("Total execution time: %.3f secs with %d cities.\n", timeElapsed/1000.0,getNCities());
+
+        return solution;
+    }
+
+    public Node Solve_Conc(int CostMatrix[][]) {
+
+    }
+
+
 
     // Add node to the queue of pending processing nodes
     public synchronized void pushNode(Node node)
--- src/TSP_Conc.java
+++ src/TSP_Conc.java
@@ -4,22 +4,29 @@ import java.util.concurrent.Executors;
 public class TSP_Conc {
 
     public static void main(String[] args) throws InterruptedException {
-        TSP tsp;
+        TSP tsp = new TSP();
         int numThreads = Runtime.getRuntime().availableProcessors(); // Valor por defecto
         Methods method = Methods.FIXED_THREAD_POOL; // Valor por defecto
+
 
         if (args.length != 3) {
             System.err.println("Use: TSP_Conc [<Cities_File>] [<Num_Threads>] [<Concurrent_Method>]");
             return;
         }
 
-        // Leer el archivo de ciudades si está presente
-        tsp = (args.length >= 1) ? new TSP(args[0]) : new TSP();
+        if (args.length == 1) {
+            try {
+                tsp = new TSP(args[0], numThreads, method);
+            } catch (IllegalArgumentException e) {
+                System.err.println("Invalid concurrence method. Switching to FixedThreadPool.");
+            }
+        }
 
         // Leer el número de hilos si está presente
-        if (args.length >= 2) {
+        if (args.length == 2) {
             try {
                 numThreads = Integer.parseInt(args[1]);
+                tsp = new TSP(args[0], numThreads, method);
             } catch (NumberFormatException e) {
                 System.err.println("Number of threads must be an integer. Using 4 threads.");
             }
@@ -28,14 +35,16 @@ public class TSP_Conc {
         // Leer el método de concurrencia si está presente
         if (args.length == 3) {
             try {
+                numThreads = Integer.parseInt(args[1]);
                 method = Methods.valueOf(args[2].toUpperCase());
+                tsp = new TSP(args[0], numThreads, method);
             } catch (IllegalArgumentException e) {
                 System.err.println("Invalid concurrence method. Switching to FixedThreadPool.");
             }
         }
 
         // Ejecutar la versión concurrente del TSP
-        tsp.executeMethod(method);
+        tsp.Solve_Conc();
     }
 
 }
